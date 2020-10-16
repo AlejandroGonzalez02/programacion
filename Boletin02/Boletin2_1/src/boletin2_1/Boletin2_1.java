227cebd863ef84611e26b6267acc76a5b372a1bb
Alejandro <agonzalezalonso@danielcastelao.org>
2020-10-16 10:12:47 +0200

    guardados todos los archivos.

---
+++ Boletin02/Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_1;
+
+/**
+ *
+ * @author dam1a
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float base=4;
+        float altura=3;
+        float area;
+        
+        area= ((base*altura)/2);
+        System.out.println("El area del triangulo es igual a "+ area + ".");
+    }
+    
+}
---
+++ Boletin02/Proyecto2_1/src/proyecto2/Proyecto2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2;
+
+/**
+ *
+ * @author dam1a
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float base= 4;
+        float altura=2;
+        float area;
+        
+        area=base*altura;
+        System.out.println("la base del cuadrado es igual a " + area + ".");
+    }
+    
+}
