48e318c8fc907cec2362299897df513363964f88
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2020-10-18 20:24:37 +0200

    Empezado Boletin 3 exercicio 1

---
+++ Boletin03/Boletin3_1/src/boletin3_1/Boletin3_1.java
@@ -0,0 +1,18 @@
+package boletin3_1;
+
+import java.util.Scanner;
+
+public class Boletin3_1 {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float tarifa, pagado;
+        System.out.println("Escriba a tarifa do producto: ");
+        tarifa= sc.nextFloat();
+        System.out.println("Inrtoduce o precio pagado: ");
+        pagado=sc.nextFloat();
+        System.out.println(" O porcetaxe de desconto é igual a " + ((100-(pagado*100)/tarifa)) + "%.");
+    }
+    
+}
